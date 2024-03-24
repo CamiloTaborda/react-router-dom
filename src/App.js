20630e83379813dbd23e71526bf83883548e8bcd
Camilo Taborda <camilotaborda2010@gmail.com>
2024-03-23 20:50:19 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Menu } from "./Menu";
+import { HomePage } from "./Components/HomePage";
+import { ProfilePage } from "./Components/ProfilePage";
+import { BlogPage } from "./Components/BlogPage";
+import { BlogPost } from "./Components/BlogPost";
+import { LoginPage } from "./Components/LoguinPage";
+import { LogoutPage } from "./Components/LogoutPage";
+import { AuthProvider, AuthRoute } from "./auth";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <HashRouter>
+    <AuthProvider>
+    <Menu />
+
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+
+    <Route path="/blog" element={<BlogPage />}>
+    <Route path=":slug" element={<BlogPost />} />
+    </Route>
+ 
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/logout" element={
+      <AuthRoute>
+        <LogoutPage />
+      </AuthRoute>
+    } />
+    <Route path="/profile" element={
+      <AuthRoute>
+        <ProfilePage />
+      </AuthRoute>
+    } />
+ 
+    <Route path="*" element={<p>Not Found</p>} />
+    </Routes>
+    </AuthProvider>
+   </HashRouter>
+   </>
   );
 }
 
---
+++ src/Components/blogdata.js
@@ -0,0 +1,21 @@
+const blogdata = [];
+blogdata.push({
+    title: 'Que es React',
+    slug: 'que-es-react',
+    content: 'React es una biblioteca JavaScript de código',
+    author: 'Camilo',
+});
+blogdata.push({
+    title: 'Que es Angula',
+    slug: 'que-es-angular',
+    content: 'Angular es un framework de desarrollo web para',
+    author: 'Cristian',
+});
+blogdata.push({
+    title: 'Que es Vue',
+    slug: 'que-es-vue',
+    content: 'Vue es una biblioteca',
+    author: 'jose',
+});
+
+export { blogdata }
